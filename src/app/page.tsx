04df52e4bdf72e689252ed848ab4ea419e46c197
Rakotomalala Fahasoavana Johann Luciano <jolu032302@gmail.com>
2024-12-02 13:28:21 +0300

    Modification de la configuration de Prettier pour désactiver les points-virgules et correction de la capitalisation de "TypeScript" dans le message d'accueil.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <><div className="flex justify-center items-center" style={{height: "100vh"}}><h1 className="h-auto">Welcome to my channel We are going to learn next.js with type script</h1></div> </>
+    <><div className="flex justify-center items-center" style={{height: "100vh"}}><h1 className="h-auto">Welcome to my channel We are going to learn next.js with TypeScript</h1></div> </>
   );
 }

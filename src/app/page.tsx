45d82cd90d07623b86a76393bf2881da0dd2f414
Rakotomalala Fahasoavana Johann Luciano <jolu032302@gmail.com>
2024-12-02 08:02:00 +0300

    Mise à jour de la configuration de Prettier et modification du titre de la page d'accueil pour inclure TypeScript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <><div className="flex justify-center align-middle py-4" style={{height: "100vh"}}><h1 className="h-auto">Welcome to my channel We are going to learn next.js with ts</h1></div> </>
+    <><div className="flex justify-center items-center" style={{height: "100vh"}}><h1 className="h-auto">Welcome to my channel We are going to learn next.js with type script</h1></div> </>
   );
 }
